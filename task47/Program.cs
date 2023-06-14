c233348eb325740c630a7f7fdc5047a6db0bdaaf
Substrata <digital_strata@mail.ru>
2023-06-14 18:13:44 +0300

    added solution for task47

--- task47/Program.cs
+++ task47/Program.cs
@@ -4,3 +4,18 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+void NewArray(int m, int n)
+{
+    double[,] array = new double[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(-9.132 + rnd.NextDouble() * 19.135, 1);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+NewArray(3, 4);
